0726b923e2d1114027de2215b9c8f1e847eeccf1
JonathanRios18 <sikra182@gmail.com>
2025-01-30 14:18:25 -0600

    migraciones

---
+++ database/migrations/2025_01_30_185625_create_owners_stores_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('owners_stores', function (Blueprint $table) {
+            $table->foreignId('owner_id')->constrained('owners');
+            $table->foreignId('store_id')->constrained('stores');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('owners_stores');
+    }
+};
---
+++ database/migrations/2025_01_30_185639_create_workers_stores_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workers_stores', function (Blueprint $table) {
+            $table->foreignId('worker_id')->constrained('workers');
+            $table->foreignId('store_id')->constrained('stores');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workers_stores');
+    }
+};
