a47bb723d5362703502e519f4cbefcd053a4037e
tovar <jesus.t.morales27@gmail.com>
2025-04-06 16:09:39 -0600

    Remove unused trigger for stock update on deliveries and clean up migration file.

--- database/migrations/2025_03_29_222239_add_triggers_to_deliveries.php
+++ database/migrations/2025_03_29_222239_add_triggers_to_deliveries.php
@@ -17,25 +17,10 @@ class AddTriggersToDeliveries extends Migration
                 WHERE id = NEW.invoice_id;
             END;
         ");
-
-        DB::unprepared("
-            CREATE TRIGGER actualizar_stock_delivery
-            AFTER UPDATE ON deliveries
-            FOR EACH ROW
-            BEGIN
-                IF NEW.status = 'Completed' THEN
-                    UPDATE products p
-                    JOIN delivery_details dd ON p.id = dd.product_id
-                    SET p.stock_weight = p.stock_weight - dd.quantity_weight
-                    WHERE dd.delivery_id = NEW.id;
-                END IF;
-            END;
-        ");
     }
 
     public function down()
     {
         DB::unprepared("DROP TRIGGER IF EXISTS asignar_estado_invoice;");
-        DB::unprepared("DROP TRIGGER IF EXISTS actualizar_stock_delivery;");
     }
 }
